af9e6083195725b9a1bb1521e5639ddd74a66ee0
Anthony Infortunio <ainfortunio1@gmail.com>
2022-11-07 23:46:50 -0500

    add a player class and player tests
    this commit adds three methods to the player class, cpuGenAttack(),
    cpuTurn() and playerTurn().
    
    -cpuGenAttack()
    generates a random integer between 0-9 for both the x and y
    coordinates. If the space has already been attacked (boardArray is
    noted by either 'M' or 'H') another random coordinate will be
    generated.
    
    -cpuTurn()
    will generate a coordinate cpuGenAttack() and then attack the player
    gameboard with those generated coordinates.
    
    -playerTurn()
    attacks the cpu gameboard with an attack parameter passed to this
    method.
    
    tests were also implimented to test that cpuGenAttack generates
    a valid coordinate and that the playerTurn registers an attack
    properly on the CpuBoard.

---
+++ src/scripts/player/player.js
@@ -0,0 +1,40 @@
+import Gameboard from '../gameboard/gameboard';
+
+class Player {
+  constructor(isPlayer) {
+    this.isPlayer = isPlayer;
+    this.Gameboard = new Gameboard();
+  }
+
+  cpuGenAttack(PlayerBoard) {
+    let validMove = false;
+    let x;
+    let y;
+    while (validMove === false) {
+      // generate a random number between 0-9 for an x coord
+      x = Math.floor(Math.random() * 10);
+      // generate a random number between 0-9 for an y coord
+      y = Math.floor(Math.random() * 10);
+      // check the boardArray for 'M' or 'H' which tells us if the space has been attacked already
+      if (PlayerBoard.boardArray[x][y] !== 'M' && PlayerBoard.boardArray[x][y] !== 'H') {
+        // if the space hasn't been attacked change validMove to true
+        validMove = true;
+      }
+    }
+    return [x, y];
+  }
+
+  cpuTurn(PlayerBoard) {
+    // generate an attack
+    const attack = this.cpuGenCoord(PlayerBoard);
+    // attack enemy gameboard
+    PlayerBoard.recieveAttack(attack);
+  }
+
+  playerTurn(CpuBoard, attack) {
+    // attack the enemy gameboard
+    CpuBoard.recieveAttack(attack);
+  }
+}
+
+export default Player;
